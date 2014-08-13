29c7e00af5f1859485eb3a191842557bdc463d76
Lazigerbill <chaukbill@gmail.com>
2014-08-13 14:18:50 -0400

    exercise5.rb is completed

--- exercise5.rb
+++ exercise5.rb
@@ -1,4 +1,13 @@
 # converts Fahrenheit temperatures to Celsius
 
 puts "Please enter the temperature you wish to convert in Fahrenheit"
-fahren=gets.chomp
+fahren=gets.chomp.to_i
+
+def temp_conversion	(value)
+	celsius=(value-32)*5/9
+	p "#{value} Fahrenheit equals to #{celsius} Celsius"
+end
+
+temp_conversion(fahren)
+
+
